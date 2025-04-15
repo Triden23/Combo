034b63119a88b81c25ac645fac7fa765e2a6d302
Nicholas Collins <ncollins.personal@gmail.com>
2025-04-14 21:34:40 -0500

    Setup for GUI use. Logic still needed

---
+++ src/Converter.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Converter {
+
+    String Path;
+    public Converter(){
+
+    }
+
+    public void setPath(String Path){
+        this.Path = Path;
+    }
+
+    public String Convert(Games Current, Games Target, String Input){
+        String converted = "";
+        return converted;
+    }
+
+}
